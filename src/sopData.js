e953b1ffb485dc27d32c39eb4e5958117974b6f5
manikandan-solartis <manikandan_ka@solartis.com>
2025-12-06 00:47:23 -0500

    Update sopData.js

--- src/sopData.js
+++ src/sopData.js
@@ -268,7 +268,6 @@ Note format: MM/DD/YYYY- Sent YYYY term to direct collections team in MM.DD.YYYY
 
 **Escalation:** surety.accounting@rlicorp.com`,
     keywords: ['shift+f1', 'collections', 'my bond center', 'f6', 'rlink3', 'wins', 'notes needed', 'direct collect', 'surety operations', 'total due']
-  }
 };
 
 ,
