a6830d5049668c8f39d41627638e00ef8ebb28fe
manikandan-solartis <manikandan_ka@solartis.com>
2025-12-06 22:43:33 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+// Master SOP Index - Import all individual SOPs here
+import agencyCheckRequest from './agency-check-request';
+import agentStatementRequests from './agent-statement-requests';
+import collectionsRlinkWins from './collections-rlink-wins-notes';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// To add a new SOP:
+// 1. Create new file: src/sops/your-sop-name.js
+// 2. Import it here: import yourSopName from './your-sop-name';
+// 3. Add to sopData object below with appropriate key
+
+export const sopData = {
+  'agency-check-request': agencyCheckRequest,
+  'agent-statement-requests': agentStatementRequests,
+  'collections-rlink-wins-notes': collectionsRlinkWins
+  // Add more SOPs here as: 'sop-id': sopVariableName,
+};
+
+export default sopData;
